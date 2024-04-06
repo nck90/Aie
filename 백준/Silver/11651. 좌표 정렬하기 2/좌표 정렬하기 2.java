a63983c1c0245c22e973e2195575520b1bd22da1
nck90 <162806035+nck90@users.noreply.github.com>
2024-04-06 15:47:27 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1756 ms, Memory: 62888 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Point[] points = new Point[N];
+
+        // 점들을 Point 객체로 생성하여 배열에 저장
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            int x = Integer.parseInt(input[0]);
+            int y = Integer.parseInt(input[1]);
+            points[i] = new Point(x, y);
+        }
+
+        // Comparator를 이용한 정렬
+        Arrays.sort(points, new Comparator<Point>() {
+            @Override
+            public int compare(Point p1, Point p2) {
+                if (p1.y == p2.y) {
+                    return Integer.compare(p1.x, p2.x);
+                }
+                return Integer.compare(p1.y, p2.y);
+            }
+        });
+
+        // 정렬된 점들을 출력
+        for (Point point : points) {
+            System.out.println(point.x + " " + point.y);
+        }
+    }
+}
